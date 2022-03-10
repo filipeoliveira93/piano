4561be3237d6dc92083d39b9a6493ada50e4d0e3
Filipe Oliveira <filipeoliveira0426@gmail.com>
2022-03-09 22:00:12 -0300

    first commit

---
+++ JS/main.js
@@ -0,0 +1,38 @@
+let keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => {
+	key.addEventListener("click", playSound);
+});
+
+function playSound(key) {
+	const audio = document.querySelector(
+		`audio[data-key="${key.target.dataset.key}"]`
+	);
+
+	audio.currentTime = 0;
+	audio.play();
+}
+
+document.addEventListener("keydown", (e) => {
+	let key = e.key;
+	const audio = document.querySelector(`audio[data-key="${key}"]`);
+	if (audio == null) {
+		return;
+	} else {
+		let keyclass = document.querySelector(`.key[data-key="${key}"]`).classList;
+		audio.currentTime = 0;
+		audio.play();
+
+		if (keyclass.contains("white")) {
+			keyclass.add("white-active");
+			document.addEventListener("keyup", () => {
+				keyclass.remove("white-active");
+			});
+		} else {
+			keyclass.add("black-active");
+			document.addEventListener("keyup", () => {
+				keyclass.remove("black-active");
+			});
+		}
+	}
+});
